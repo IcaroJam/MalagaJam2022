4c063a3d1efaaaf81368782a392ea01d4fb91395
edfolk <96429557+edfolk@users.noreply.github.com>
2022-07-30 10:14:50 +0200

    updated

--- code/scenes/principal.js
+++ code/scenes/principal.js
@@ -2,79 +2,95 @@ class Principal extends Phaser.Scene {
 
     constructor(cursors, player) {
         super();
-
         this.cursors = cursors;
         this.player = player;
         this.laserGroup;
     }
 
     preload() {
-        // Load game resources
-        this.load.image('tiles', 'assets/sheet_7.png');
-        this.load.spritesheet("characters", "assets/characters.png", { frameWidth: 32, frameHeight: 32 });
-        // Load the export Tiled JSON
-        this.load.tilemapTiledJSON('map', 'assets/mapa.json');
-        this.load.image('laser', "assets/fire.png");
+        // Tileset inicial de Nestor
+        this.load.image('tiles', 'assets/Untitled.png');
+        // Tileset de prueba
+        this.load.spritesheet("characters", "assets/zombie.png", { frameWidth: 64, frameHeight: 128 });
+        // Mapa en formato JSON creado con Tiled
+        this.load.tilemapTiledJSON('map', 'assets/prueba_jam2.json');
+        // Fondos
+        this.load.image('fondo', 'assets/StaticBG1.png');
+        this.load.image('montana', 'assets/DynamicBG1.png');
+        this.load.image('laser', "assets/fire.png")
     }
 
     create() {
-        // 1st run to set everything in place
+        // Asignacion de los cursores direccion
         this.cursors = this.input.keyboard.createCursorKeys();
-        this.cameras.main.setBounds(0, 0, 800 * 2, 600 * 2);
-        this.physics.world.setBounds(0, 0, 800 * 2, 600 * 2);
 
+        const zoom = 2;
+        // Reajuste de las camaras y el mundo para el zoom * 2
+        this.cameras.main.setBounds(0, 0, 1280 * zoom, 1024 * zoom);
+        this.physics.world.setBounds(0, 0, 1280 * zoom, 1024 * zoom);
+
+        this.add.image(1000, 1000, 'fondo').setScale(4);
+        this.add.image(1000, 1500, 'montana').setScale(2);
+
+        // NOTA: Las llaves terreno_nivel1 (tileset) y terreno (layer) seasignan en Tiled
+        // Creamos el mapa a traves del objeto de la configuración
         const map = this.make.tilemap({ key: 'map' })
-        const tileset = map.addTilesetImage('platformer', 'tiles');
-        const platforms = map.createLayer('platforms', tileset, 0, 0);
-        const water = map.createLayer('water', tileset, 0, 0);
-        platforms.setCollisionByExclusion(-1, true);
-        platforms.setScale(2);
-        water.setScale(2);
 
+        // Le asignamos su tileset para que lo pueda dibujar
+        const tileset = map.addTilesetImage('terreno_nivel1', 'tiles');
+
+        // Creamos una capa cargada desde la config del map
+        const platforms = map.createLayer('terreno', tileset, 0, 800);
+        platforms.setScale(1);
+
+        // Importante para la colision
+        platforms.setCollisionByExclusion(-1, true);
+
+        // Añadimos al jugador
         this.player = this.physics.add.sprite(100, 800, 'characters');
-        this.player.setBounce(0.01);
-        this.player.setCollideWorldBounds(true);
-        this.physics.add.collider(this.player, platforms);
-        this.player.body.setGravityY(250)
-        this.player.setScale(2);
+        this.player.setBounce(0.1);
+        this.player.body.setGravityY(150)
+        this.player.setScale(1);
 
         this.laserGroup = new LaserGroup(this);
 		this.addEvents();
 
+        // Colisiones del jugador
+        this.player.setCollideWorldBounds(true);
+        this.physics.add.collider(this.player, platforms);
+
+        // Animación para andar
         this.anims.create({
             key: 'walk',
-            frames: this.anims.generateFrameNumbers('characters', { start: 0, end: 4 }),
+            frames: this.anims.generateFrameNumbers('characters', { start: 0, end: 2 }),
             frameRate: 5,
             repeat: -1
         });
 
+        // La camera sigue al jugador
         this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
     }
 
     update() {
-        // Function used to update game assets like our character or to read the pressed keys
-        this.player.anims.play('walk', true);
+        const velocity = 200
         if (this.cursors.left.isDown) {
             this.player.setVelocityX(-240);
-        
             this.player.anims.play('walk', true);
         }
         else if (this.cursors.right.isDown) {
             this.player.setVelocityX(240);
-        
             this.player.anims.play('walk', true);
         }
         else {
             this.player.setVelocityX(0);
             this.player.anims.stop('walk');
         }
-        
+
         if (this.cursors.up.isDown && this.player.body.onFloor()) {
             this.player.setVelocityY(-330);
         }
     }
 
-	
     addEvents() {
 	    this.input.on('pointerdown', pointer => {
             this.shootLaser();
